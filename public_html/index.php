07001e145fb439b44598237fb204fa5845e7b5e4
Tomislav <44715559+TVCTC@users.noreply.github.com>
2022-10-08 11:31:37 +0200

    Update and rename index.htm to index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,6 @@
-//VIEW: https://livestreamserver.000webhostapp.com/
+//GAS: https://script.google.com/home/projects/1ZEygjvxxCtVGCBdrtbR68i6y3bzm9gDFGibXzvkANvsPsOEpgjhZBJDX/edit
 //EDIT: https://files.000webhost.com/
+//VIEW: https://livestreamserver.000webhostapp.com/
 <?php
 $url = "https://tinyurl.com/esawebtvone";
 $m3u8_url = file_get_contents($url);
