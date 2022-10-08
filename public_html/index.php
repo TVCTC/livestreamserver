85807d41bb27a7896d84d2761a4001e025d3e27f
Tomislav <44715559+TVCTC@users.noreply.github.com>
2022-10-08 09:18:59 +0200

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -4,18 +4,3 @@ $m3u8_url = file_get_contents($url);
 header("Location: " . $m3u8_url);
 exit();
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>My first PHP script</title>
-        <style type="text/css">
-            body {background:black; color:lightgreen}
-        </style>
-    </head>
-    <body>
-    <?php
-        echo 'PHP version: ' . phpversion() . '\r\n';
-        echo $m3u8_url;
-    ?>
-    </body>
-</html>
